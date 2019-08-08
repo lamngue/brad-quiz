365adb99f2f1b8d3f00e4170ee13cc3bf7e9d6ca
Lam Nguyen <lnguye16@gustavus.edu>
2019-08-08 16:27:51 +0700

    update

--- src/Screens/Utils/Api.js
+++ src/Screens/Utils/Api.js
@@ -1,11 +1,11 @@
 import * as axios from 'axios';
 
 const instance = axios.default.create({
-    baseURL: process.env.REACT_APP_BASE_URL,
+    baseURL: "https://brad-quiz-backend.herokuapp.com",
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
-        "Access-Control-Allow-Origin": process.env.REACT_APP_BASE_URL
+        "Access-Control-Allow-Origin": "https://brad-quiz-backend.herokuapp.com"
     }
 });
 
