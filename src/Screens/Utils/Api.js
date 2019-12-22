e5dcc256604ed7873b256ee276b3abc122f65fc5
Lam Nguyen <lnguye16@gustavus.edu>
2019-12-22 11:46:42 -0600

    update

--- src/Screens/Utils/Api.js
+++ src/Screens/Utils/Api.js
@@ -1,7 +1,7 @@
 import * as axios from 'axios';
 
 const instance = axios.default.create({
-    baseURL: "https://competent-easley-33a50c.netlify.com",
+    baseURL: "https://brad-quiz-backend.herokuapp.com/",
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json'
